aa34345127cd912129c534148ec643e041a7b742
Felix <felix.gagne.cliche@gmail.com>
2022-04-12 05:53:05 -0400

    remove theme

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,13 +1,8 @@
-import { nightOwl } from 'strapi-code-themes';
-
 export default {
   config: {
     locales: [
       'fr'
-    ],
-    theme: {
-      colors: nightOwl,
-    },
+    ]
   },
   bootstrap(app) {
     console.log(app);
